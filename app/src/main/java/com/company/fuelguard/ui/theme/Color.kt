ee0c16085c91d9e8040028dfaab920c9bbcb984b
kgsolutionsp-cpu <kg.solutionsp@gmail.com>
2026-07-23 16:00:39 +0100

    Create Color.kt

---
+++ app/src/main/java/com/company/fuelguard/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.company.fuelguard.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF1A73E8)
+val SecondaryDark = Color(0xFF202124)
+val White = Color(0xFFFFFFFF)
+val LightGrey = Color(0xFFF1F3F4)
+val TextBlack = Color(0xFF202124)
+val ErrorRed = Color(0xFFD93025)
+val WarningOrange = Color(0xFFFFA000)
+
+// Dark Theme Colors
+val DarkPrimary = Color(0xFF8AB4F8)
+val DarkBackground = Color(0xFF202124)
+val DarkSurface = Color(0xFF3C4043)
+val DarkOnPrimary = Color(0xFF202124)
+val DarkOnBackground = Color(0xFFE8EAED)
+val DarkOnSurface = Color(0xFFE8EAED)
